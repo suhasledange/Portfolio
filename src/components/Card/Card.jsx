9754e4c46d0ada994250c17e10808a6038e50d29
suhasledange <suhasledange10@gmail.com>
2024-01-16 17:54:46 +0530

    update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import { IoIosOpen } from "react-icons/io";
 const Card = ({heading,TechStack,Img,para,git,live,liveText,year}) => {
   return (
     <div className='relative border bg-gray-50 rounded-md shadow-md pb-2 cursor-pointer mx-auto w-[90%] md:w-full overflow-hidden group'>
-    <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-0 group-hover:opacity-90 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 flex items-center justify-center md:gap-10 gap-5'>
+    <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-0 group-hover:opacity-90 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 flex items-center justify-center lg:flex-row flex-col md:gap-10 gap-6'>
       <div className='absolute top-5 left-5'>
             <h1 className='text-gray-100 text-xl'>{year}</h1>
       </div>
@@ -27,8 +27,8 @@ const Card = ({heading,TechStack,Img,para,git,live,liveText,year}) => {
         {liveText} <IoIosOpen />
       </a>
     </div>
-    <div className='p-5'>
-      <div className='w-[90%] mx-auto h-[17rem] py-5 overflow-hidden'>
+    <div className='p-3 md:p-5'>
+      <div className='w-full md:w-[90%] mx-auto h-[17rem] py-5 overflow-hidden'>
         <img className='w-full h-full bg-cover object-contain rounded-lg' src={Img} alt={heading} />
       </div>
       <div className='space-y-3 mt-4'>
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -28,25 +28,25 @@ const Work = () => {
   return (
 
     <Container>
-      <section className='h-[37rem] mb-10 flex justify-between md:mx-0 mx-4 flex-col flex-grow'>
+      <section className='h-[35rem] md:h-[37rem] mb-10 flex justify-between mx-4 flex-col flex-grow'>
 
         <div className='flex w-full justify-between items-center h-full'>
 
           <div className='space-y-14'>
             <div className='space-y-3'>
 
-              <h1 className='text-[3.4rem] custom-font font-bold'>HELLO, I'M SUHAS</h1>
+              <h1 className='text-4xl md:text-[3.4rem] custom-font font-bold'>HELLO, I'M SUHAS</h1>
 
               <div className='h-[3rem]'>
-                <span className='text-purple-600 font-semibold text-4xl cursor-none' id="typed-text"></span>
+                <span className='text-purple-600 font-semibold text-3xl md:text-4xl cursor-none' id="typed-text"></span>
               </div>
               <div className='pt-2 md:w-2/4'>
-                <p className='text-lg font-normal text-gray-600'>I'm a passionate web developer specializing in React, where I bring creative and dynamic ideas to life through clean and efficient code and Beyond coding, I'm also a content creator on YouTube</p>
+                <p className='text-md md:text-lg font-normal text-gray-600'>I'm a passionate web developer specializing in React, where I bring creative and dynamic ideas to life through clean and efficient code and Beyond coding, I'm also a content creator on YouTube</p>
               </div>
             </div>
 
             <div className=''>
-              <a href={pdf} download="SuhasLedangeResume.pdf" className='text-2xl cursor-pointer hover:text-gray-100 font-medium border-2 hover:bg-purple-700 border-purple-700 text-purple-700 hover:shadow-lg hover:bg-transparent px-5 py-2 duration-200 '>Resume</a>
+              <a href={pdf} download="SuhasLedangeResume.pdf" className='text-xl md:text-2xl cursor-pointer hover:text-gray-100 font-medium border-2 hover:bg-purple-700 border-purple-700 text-purple-700 hover:shadow-lg hover:bg-transparent px-5 py-2 duration-200 '>Resume</a>
             </div>
           </div>
 
@@ -65,7 +65,7 @@ const Work = () => {
 
 
       <section id='work'>
-        <div className='grid md:p-0 p-4 md:grid-cols-2 gap-10'>
+        <div className='grid p-0 md:grid-cols-2 gap-10'>
           {CardData.map((item) => (
             <Card key={item.head} heading={item.head} TechStack={item.TechStack} Img={item.img} para={item.para} git={item.gitLink} live={item.liveLink} liveText={item.liveText} year={item.year} />
           ))}
