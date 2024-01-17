ffd85294ff7a2367794e9487ea0982781693c20d
suhasledange <suhasledange10@gmail.com>
2024-01-17 15:16:11 +0530

    card fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,32 +4,15 @@ import { IoIosOpen } from "react-icons/io";
 
 const Card = ({heading,TechStack,Img,para,git,live,liveText,year}) => {
   return (
-    <div className='relative border bg-gray-50 rounded-md shadow-md pb-2 cursor-pointer mx-auto w-[90%] md:w-full overflow-hidden group'>
-    <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-0 group-hover:opacity-90 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 flex items-center justify-center lg:flex-row flex-col md:gap-10 gap-6'>
-      <div className='absolute top-5 left-5'>
-            <h1 className='text-gray-100 text-xl'>{year}</h1>
+    <div className='relative border bg-gray-50 rounded-md shadow-md pb-4 pt-2 cursor-pointer mx-auto w-[90%] md:w-full overflow-hidden group flex flex-col justify-between'>
+   
+   <div className='absolute top-[0.6rem] left-3'>
+        <h1 className='text-purple-600 text-md'>{year}</h1>
       </div>
-      
-      <a
-        className='text-xl text-white font-medium hover:underline underline-offset-2 flex items-center justify-center gap-1'
-        href={git}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FaGithub /> GitHub Repository
-      </a>
-      <a
-        className='text-white font-medium text-xl hover:underline underline-offset-2 flex items-center justify-center gap-1'
-        href={live}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        {liveText} <IoIosOpen />
-      </a>
-    </div>
+
     <div className='p-3 md:p-5'>
-      <div className='w-full md:w-[90%] mx-auto h-[17rem] py-5 overflow-hidden'>
-        <img className='w-full h-full bg-cover object-contain rounded-lg' src={Img} alt={heading} />
+      <div className=' hover:scale-105 duration-300 hover:shadow-lg rounded-lg w-full md:w-[90%] mx-auto h-[17rem] py-5 overflow-hidden'>
+        <img className='w-full h-full bg-cover object-contain rounded-lg ' src={Img} alt={heading} />
       </div>
       <div className='space-y-3 mt-4'>
         <h1 className='text-xl text-gray-800 font-medium'>
@@ -40,7 +23,29 @@ const Card = ({heading,TechStack,Img,para,git,live,liveText,year}) => {
         </h3>
         <p className='text-gray-600'>{para}</p>
       </div>
+      
+     
     </div>
+   
+    <div className=' ml-5 flex items-center space-x-5'>
+     <a
+        className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
+        href={git}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaGithub /> GitHub
+      </a>
+      <a
+        className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
+        href={git}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <IoIosOpen /> Live
+      </a>
+    </div>
+  
   </div>
   )
 }
