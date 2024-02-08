71c959becf9e490278db6a9c716dcaa9535f9d36
suhasledange <suhasledange10@gmail.com>
2024-02-08 17:23:13 +0530

    lazy load

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,52 +1,65 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
 import { IoIosOpen } from "react-icons/io";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const Card = ({heading,TechStack,Img,para,git,live,liveText,year}) => {
+
+const Card = ({ heading, TechStack, Img, para, git, live, liveText, year }) => {
   return (
     <div className='relative border dark:shadow-xl dark:drop-shadow-xl dark:border-gray-800 dark:bg-gray-800 bg-gray-50 rounded-md shadow-md pb-4 pt-2 cursor-pointer mx-auto w-[90%] md:w-full overflow-hidden group flex flex-col justify-between'>
-   
-   <div className='absolute top-[0.6rem] left-3'>
+
+      <div className='absolute top-[0.6rem] left-3'>
         <h1 className='text-purple-600 text-md'>{year}</h1>
       </div>
 
-    <div className='p-3 md:p-5'>
-      <div className=' rounded-lg w-full md:w-[90%] mx-auto h-[17rem] py-5 overflow-hidden'>
-        <img className='hover:scale-105 duration-300 dark:brightness-75 dark:contrast-125 w-full h-full bg-cover object-contain rounded-lg ' src={Img} alt={heading} />
+      <div className='p-3 md:p-5'>
+        <div className=' rounded-lg w-full md:w-[90%] mx-auto h-[17rem] py-5 hover:scale-105 duration-300'>
+          {/* <img className='hover:scale-105 duration-300 dark:brightness-75 dark:contrast-125 w-full h-full bg-cover object-contain rounded-lg ' src={Img} alt={heading} /> */}
+          
+          <LazyLoadImage
+            className='dark:brightness-75 dark:contrast-125 bg-cover object-contain rounded-lg hover:shadow-xl'
+            alt={heading}
+            src={Img}
+            effect="blur" 
+            threshold={200}
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className='space-y-3 mt-4'>
+          <h1 className='text-xl dark:text-gray-200 text-gray-800 font-medium'>
+            Project : <span className=' font-normal text-gray-700 dark:text-gray-400'>{heading}</span>{' '}
+          </h1>
+          <h3 className='font-medium dark:text-gray-200 text-gray-800 text-md'>
+            Technology : <span className='dark:text-gray-400 text-gray-700 text-sm italic'>{TechStack}</span>{' '}
+          </h3>
+          <p className='text-gray-600 dark:text-gray-500'>{para}</p>
+        </div>
+
+
       </div>
-      <div className='space-y-3 mt-4'>
-        <h1 className='text-xl dark:text-gray-200 text-gray-800 font-medium'>
-          Project : <span className=' font-normal text-gray-700 dark:text-gray-400'>{heading}</span>{' '}
-        </h1>
-        <h3 className='font-medium dark:text-gray-200 text-gray-800 text-md'>
-          Technology : <span className='dark:text-gray-400 text-gray-700 text-sm italic'>{TechStack}</span>{' '}
-        </h3>
-        <p className='text-gray-600 dark:text-gray-500'>{para}</p>
+
+      <div className=' ml-5 flex items-center space-x-5'>
+        <a
+          className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
+          href={git}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaGithub /> GitHub
+        </a>
+        <a
+          className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
+          href={live}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <IoIosOpen /> {liveText}
+        </a>
       </div>
-      
-     
+
     </div>
-   
-    <div className=' ml-5 flex items-center space-x-5'>
-     <a
-        className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
-        href={git}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FaGithub /> GitHub
-      </a>
-      <a
-        className='text-lg duration-200 text-purple-600 hover:text-purple-800 font-medium hover:underline underline-offset-2 flex items-center gap-1'
-        href={live}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <IoIosOpen /> {liveText}
-      </a>
-    </div>
-  
-  </div>
   )
 }
 
--- src/components/Youtube/YoutubeCard.jsx
+++ src/components/Youtube/YoutubeCard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Ylogo } from '../../assets';
 import ReactPlayer from 'react-player/lazy';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const YoutubeCard = ({ title, link }) => {
   const [isPlayerReady, setPlayerReady] = useState(false);
@@ -10,33 +12,45 @@ const YoutubeCard = ({ title, link }) => {
   };
 
   const playerStyle = {
-    borderRadius: '6px', 
+    borderRadius: '6px',
     overflow: 'hidden',
   };
 
 
   return (
     <div className='lg:w-72 md:w-80 w-96 mx-auto mb-24 lg:h-40 md:h-44 h-48'>
-      
+
       <div className='mx-auto w-full h-full'>
 
-      {!isPlayerReady && <div className='flex items-center justify-center bg-black w-full h-full'>
+        {!isPlayerReady && <div className='flex items-center justify-center bg-black w-full h-full'>
           <div className='animate-spin rounded-full border-t-2 border-b-2 border-purple-700 h-12 w-12'></div>
         </div>}
 
-      <ReactPlayer 
-        style={playerStyle}
-      width="100%"
-        height="100%"
-        url={link}
-        onReady={handlePlayerReady}
+        <ReactPlayer
+          style={playerStyle}
+          width="100%"
+          height="100%"
+          url={link}
+          onReady={handlePlayerReady}
         />
 
       </div>
 
       <div className='flex items-center space-x-4 mt-2'>
         <div className='w-10 h-10'>
-          <img className='w-full h-full object-contain' src={Ylogo} alt='YouTube Logo' />
+
+          {/* <img className='w-full h-full object-contain' src={Ylogo} alt='YouTube Logo' /> */}
+
+          <LazyLoadImage
+            alt=""
+            src={Ylogo}
+            effect="blur" 
+            threshold={200}
+            width="100%"
+            height="100%"
+            style={{ objectFit: 'contain' }}
+          />
+
         </div>
         <h1 className='text-md font-normal text-gray-700 dark:text-gray-200'>
           {title.substr(0, 50)}...
