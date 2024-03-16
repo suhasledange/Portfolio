fb8129c46376b1a86b9b31a5f4e559b1122a49f7
suhasledange <suhasledange10@gmail.com>
2024-03-16 13:39:57 +0530

    header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,31 +22,34 @@ const Header = () => {
 
   return (
     <header className='w-full bg-gray-50 dark:bg-gray-800 md:shadow-md shadow-sm dark:drop-shadow-md sticky top-0 z-30'>
+
+      <div className='flex px-3 justify-between items-center mx-auto max-w-screen-lg h-[4rem]  md:h-[4.5rem] relative'>
+
       <div onClick={()=>setMenuOpen(false)} className={`${menuOpen ? "translate-x-0" : "translate-x-full"} absolute md:top-[4.5rem] top-16 bg-transparent w-full min-h-screen`}></div>
      
-      <div
-          className={` ${menuOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-200  lg:hidden absolute bg-gray-50 border-t-2 border-gray-100 dark:bg-gray-800 w-[60%] min-h-screen top-16 right-0 shadow-lg z-10 `}
-        >
-          
-          <ul className=' tracking-wider text-lg flex flex-col gap-5 items-center pt-4'>
-            {Navlinks.map((item) => (
-              <NavLink
-                key={item.text}
-                to={item.link}
-                onClick={() => setMenuOpen(false)}
-                className={({ isActive }) =>
-                  `flex items-center mt-5 h-full px-4 font-medium text-md ${isActive ? 'text-purple-700' : 'text-gray-500 '
-                  }`
-                }
-              >
-                {item.text}
-              </NavLink>
-            ))}
-            <li onClick={themeToggle} className='mt-20 hover:animate-pulse text-2xl cursor-pointer'>{themeMode === 'light' ? <MdDarkMode/> :<MdLightMode className='text-purple-100 '/> } </li>
-          </ul>
-        </div>
+     <div
+         className={` ${menuOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-200  lg:hidden absolute bg-gray-50 border-t-2 border-gray-100 dark:bg-gray-800 w-[60%] min-h-screen top-16 right-0 shadow-lg z-10 `}
+       >
+         
+         <ul className=' tracking-wider text-lg flex flex-col gap-5 items-center pt-4'>
+           {Navlinks.map((item) => (
+             <NavLink
+               key={item.text}
+               to={item.link}
+               onClick={() => setMenuOpen(false)}
+               className={({ isActive }) =>
+                 `flex items-center mt-5 h-full px-4 font-medium text-md ${isActive ? 'text-purple-700' : 'text-gray-500 '
+                 }`
+               }
+             >
+               {item.text}
+             </NavLink>
+           ))}
+           <li onClick={themeToggle} className='mt-20 hover:animate-pulse text-2xl cursor-pointer'>{themeMode === 'light' ? <MdDarkMode/> :<MdLightMode className='text-purple-100 '/> } </li>
+         </ul>
+       </div>
+        
 
-      <div className='flex px-3 justify-between items-center mx-auto max-w-screen-lg h-[4rem]  md:h-[4.5rem]'>
         
         <div onClick={()=>setMenuOpen(false)}>
           <h1 className='dark:text-purple-100 cursor-pointer text-purple-700 font-bold text-2xl'>Suhas Ledange</h1>
