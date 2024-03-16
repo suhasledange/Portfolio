f57ff0ad623a598dad810f277df3dbf3dd01dc89
suhasledange <suhasledange10@gmail.com>
2024-03-16 12:26:34 +0530

    container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,5 @@
 
-const Container = ({children})=>{
-    return <div className='w-full px-3 flex flex-col max-w-screen-lg mx-auto dark:bg-[#222C39] overflow-x-hidden'>{children}</div>;
+const Container = ({children,className})=>{
+    return <div className={`w-full px-3 flex flex-col max-w-screen-lg mx-auto dark:bg-[#222C39] overflow-x-hidden ${className || ""}`}>{children}</div>;
 }
 export default Container;
