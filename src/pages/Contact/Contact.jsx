b7c81250412bf27c28882d29db78bf478b19ecef
suhasledange <suhasledange10@gmail.com>
2024-03-16 15:47:26 +0530

    con

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,7 +3,6 @@ import { touch } from '../../assets';
 import conf from '../../conf/conf';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import { Container } from '../../components';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
